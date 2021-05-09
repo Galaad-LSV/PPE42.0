614cafea9d89465bfb84a00f86416725f11aecec
galaa <galaa@DESKTOP-OILG4VC>
2021-05-09 18:14:19 +0200

    Ajoutez des fichiers projet.

---
+++ PPE4/PPE4/Services/Api.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PPE4.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE4.Services
+{
+    public class Api
+    {
+        #region Restaurant
+        public async Task GetAllRestaurants(string paramUrl)
+        {
+            try
+            {
+                var clientHttp = new HttpClient();
+                var json = await clientHttp.GetStringAsync(Contstante.BaseApiAddress + paramUrl);
+                JsonConvert.DeserializeObject<List<Restaurant>>(json);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+
+        }
+        public async Task GetAllUtilisateurs(string paramUrl)
+        {
+            try
+            {
+
+
+                var clientHttp = new HttpClient();
+                var json = await clientHttp.GetStringAsync(Contstante.BaseApiAddress + paramUrl);
+                JsonConvert.DeserializeObject<List<Utilisateur>>(json);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+
+        }
+        public async Task GetAllMenus(string paramUrl)
+        {
+            try
+            {
+
+
+                var clientHttp = new HttpClient();
+                var json = await clientHttp.GetStringAsync(Contstante.BaseApiAddress + paramUrl);
+                JsonConvert.DeserializeObject<List<Menu>>(json);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+
+        }
+        public async Task<bool> GetCategorie(string param)
+        {
+            try
+            {
+                JObject oJsonObject = new JObject();
+                oJsonObject.Add("Libelle", param);
+
+                var client = new HttpClient();
+                var Content = new StringContent(oJsonObject.ToString());
+                var response = await client.PostAsync(Contstante.BaseApiAddress + "api/testCategorie", Content);
+                var content = await response.Content.ReadAsStringAsync();
+                if (content.Contains("ok"))
+                {
+
+                    return true;
+                }
+                else { return false; }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> PostUtilisateur(string Param1, string Param2)
+        {
+            try
+            {
+                JObject oJsonObject = new JObject();
+                oJsonObject.Add("Nom", Param1);
+                oJsonObject.Add("Mdp", Param2);
+                var client = new HttpClient();
+                var Content = new StringContent(oJsonObject.ToString());
+                var response = await client.PostAsync(Contstante.BaseApiAddress + "api/PostUtilisateur", Content);
+                var content = await response.Content.ReadAsStringAsync();
+                if (content.Contains("ok"))
+                {
+
+                    return true;
+                }
+                else { return false; }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+        public async Task GetAuthAsync(string param)
+        {
+            try
+            {
+                JObject oJsonOBjetct = nnew Obkect
+            }
+        }
+        #endregion
+    }
+}
---
+++ PPE4/PPE4/VuesModeles/AuthentificationVueModele.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PPE4.Models;
+using PPE4.Services;
+using Xamarin.Forms;
+
+namespace PPE4.VuesModeles
+{
+    public class AuthentificationVueModele : BaseVueModele
+    {
+        #region Attributs
+        private string _identifiant;
+        private string _motDePasse;
+        #endregion
+        #region Constructeurs
+        public AuthentificationVueModele()
+        {
+            CommandeButtonLogIn = new Command(ActionPage);
+        }
+        #endregion
+        #region Getters/Setters
+        public ICommand CommandeButtonLogIn { get; }
+        public string Identifiant
+        {
+            get
+            {
+                return _identifiant;
+            }
+            set
+            {
+                if (_identifiant != value)
+                {
+                    _identifiant = value;
+                    OnPropertyChanged(nameof(Identifiant));
+                }
+            }
+        }
+        public string MotDePasse
+        {
+            get
+            {
+                return _motDePasse;
+            }
+            set
+            {
+                if (_motDePasse != value)
+                {
+                    _motDePasse = value;
+                    OnPropertyChanged(nameof(MotDePasse));
+                }
+            }
+        }
+        #endregion
+        #region Methodes
+
+
+        public void ActionPage()
+        {
+            if (Utilisateur.CollClasse.FindAll(x => x.Nom == Identifiant).Exists(x => x.Mdp == MotDePasse)) Application.Current.MainPage = new NavigationPage(new ListingPage());
+        }
+        #endregion
+    }
+}
